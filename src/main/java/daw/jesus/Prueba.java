992b626603e43b7ffbb798e5b65b2c4444838f96
jesusledesma15 <jesusproint998@gmail.com>
2020-01-11 19:24:48 +0100

    Añade objeto jugador

---
+++ src/main/java/daw/jesus/Prueba.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daw.jesus;
+
+/**
+ *
+ * @author Jesus
+ */
+public class Prueba {
+    
+    public static void main(String[] args) {
+        
+        Jugador j1 = new Jugador("Jesus", 1000, 1);
+    }
+}
